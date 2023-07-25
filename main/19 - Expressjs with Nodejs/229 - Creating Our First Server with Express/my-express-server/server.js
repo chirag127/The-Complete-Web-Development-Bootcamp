f01e237a5cfc6f28bda9bee05ee9935499828845
Nitesh <85004498+Niteshmeena9672@users.noreply.github.com>
2023-07-25 10:23:58 +0530

    Update server.js

--- main/19 - Expressjs with Nodejs/229 - Creating Our First Server with Express/my-express-server/server.js
+++ main/19 - Expressjs with Nodejs/229 - Creating Our First Server with Express/my-express-server/server.js
@@ -14,3 +14,7 @@ app.listen(3300, function() {
 // This code imports the Express module and creates an Express application.
 // The application is then configured to listen on port 3300.
 // When the application starts, a message is logged to the console.
+
+app.get("/",(req ,res)=>{
+    res.send("You are at Homepage");
+})

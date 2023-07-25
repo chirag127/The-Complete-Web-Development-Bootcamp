b1d431016319a734f69af5d6b2e377c94ee47d2d
nitesh <niteshmeena9672@gmail.com>
2023-07-25 11:22:50 +0530

    express with node completed

--- main/19 - Expressjs with Nodejs/230 - Handling Requests and Responses the GET Request/my-express-server/server.js
+++ main/19 - Expressjs with Nodejs/230 - Handling Requests and Responses the GET Request/my-express-server/server.js
@@ -1,34 +1,32 @@
-// Import the 'express' module
-import express from "express";
-
-// Create an instance of the Express application
+import express from 'express';
 const app = express();
-
-// Set the port number to 3000
 const port = 3000;
 
-app.listen(port, function () {
-    console.log("Server started on port 3000");
-  });
-  
-  // Handle the root route ("/") and send a response
-  app.get("/", function (req, res) {
-    console.log(req);
-    res.send("<h1>Hello, World!</h1>");
-  });
-  
-  // Handle the "/contact" route and send a response
-  app.get("/contact", function (req, res) {
-    res.send("Contact me at: 239847239847");
-  });
-  
-  // Handle the "/about" route and send a response
-  app.get("/about", function (req, res) {
-    res.send("My name is Nitesh meena;");
-  });
-  
-  // Handle the "/hobbies" route and send a response with a list of hobbies
-  app.get("/hobbies", function (req, res) {
-    res.send("<ul><li>Coffee</li><li>Code</li><li>More Coffee</li></ul>");
-  });
-  
+// Handling GET Request
+app.get('/', (req, res) => {
+  res.send('Hello World - GET');
+});
+
+// Handling POST Request
+app.post('/', (req, res) => {
+  res.send('Hello World - POST');
+});
+
+// Handling PUT Request
+app.put('/', (req, res) => {
+  res.send('Hello World - PUT');
+});
+
+// Handling PATCH Request
+app.patch('/', (req, res) => {
+  res.send('Hello World - PATCH');
+});
+
+// Handling DELETE Request
+app.delete('/', (req, res) => {
+  res.send('Hello World - DELETE');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
